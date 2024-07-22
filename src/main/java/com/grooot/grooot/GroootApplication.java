d6165fa398bf5d72aed71447c24a5f04c7ee905c
anikchak <user@users-MacBook-Pro.local>
2024-07-22 23:43:57 +0530

    pushing skeleton structure

---
+++ src/main/java/com/grooot/grooot/GroootApplication.java
@@ -0,0 +1,13 @@
+package com.grooot.grooot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grooot/grooot/GroootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grooot.grooot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
